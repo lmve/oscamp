494e8691a5f927c9ed2e8b71f944da7e0944c918
yuchen <yuchen@tsinghua.edu.cn>
2025-04-22 16:00:33 +0800

    update arceos/modules/bump_allocator/src/lib.rs for rust analyzer

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -16,16 +16,73 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator;
+pub struct EarlyAllocator<const SIZE: usize> {}
 
-impl EarlyAllocator {
+impl<const SIZE: usize> EarlyAllocator<SIZE> {
+    pub const fn new() -> Self {
+        Self {}
+    }
 }
 
-impl BaseAllocator for EarlyAllocator {
+impl<const SIZE: usize> BaseAllocator for EarlyAllocator<SIZE> {
+    fn init(&mut self, start: usize, size: usize) {
+        todo!()
+    }
+
+    fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
+        todo!()
+    }
 }
 
-impl ByteAllocator for EarlyAllocator {
+impl<const SIZE: usize> ByteAllocator for EarlyAllocator<SIZE> {
+    fn alloc(
+        &mut self,
+        layout: core::alloc::Layout,
+    ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
+        todo!()
+    }
+
+    fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
+        todo!()
+    }
+
+    fn total_bytes(&self) -> usize {
+        todo!()
+    }
+
+    fn used_bytes(&self) -> usize {
+        todo!()
+    }
+
+    fn available_bytes(&self) -> usize {
+        todo!()
+    }
 }
 
-impl PageAllocator for EarlyAllocator {
+impl<const SIZE: usize> PageAllocator for EarlyAllocator<SIZE> {
+    const PAGE_SIZE: usize = SIZE;
+
+    fn alloc_pages(
+        &mut self,
+        num_pages: usize,
+        align_pow2: usize,
+    ) -> allocator::AllocResult<usize> {
+        todo!()
+    }
+
+    fn dealloc_pages(&mut self, pos: usize, num_pages: usize) {
+        todo!()
+    }
+
+    fn total_pages(&self) -> usize {
+        todo!()
+    }
+
+    fn used_pages(&self) -> usize {
+        todo!()
+    }
+
+    fn available_pages(&self) -> usize {
+        todo!()
+    }
 }
