159dbb810cf1b104ca87e9d4735b5ef8327d87c1
lmve <1831232566@qq.com>
2025-07-17 01:28:32 +0800

    Print with color.       add macro print_color!    ulib/axstd/src/macros.rs
    Signed-off-by: lmve <1831232566@qq.com>

--- arceos/exercises/print_with_color/src/main.rs
+++ arceos/exercises/print_with_color/src/main.rs
@@ -3,8 +3,13 @@
 
 #[cfg(feature = "axstd")]
 use axstd::println;
+use axstd::print_color;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    println!("[WithColor]: Hello, Arceos!");
+    // print_color!("\x1b[34m", "\x1b[0m", "[WithColor]: Hello, Arceos!");
+    print_color!("#0000FF", "#c545ccff", "[WithColor]: Hello, Arceos!");
+    print_color!("#7dac5bff", "#206c8dff", "[WithColor]: Hello, Arceos!");
+    print_color!("#e46697ff", "#901616ff", "[WithColor]: Hello, Arceos!");
+    print_color!("#7f5620ff", "#4c9c1bff", "[WithColor]: Hello, Arceos!");
 }
